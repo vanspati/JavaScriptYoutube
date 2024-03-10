5ac2106f0a68a169e5b87259563aebffc99c64b9
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-03-11 01:55:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// // console.log(`suraj S. Jondhale`);
+// // console.log("mask");
+// // // alert("this is alert box testing");
+// // document.getElementById("myh1").textContent="mumma" ; 
+// // // first lecture
+
+
+
+// // // introducton to variables :
+
+// let price=200;
+// let marks=56;
+// // let age=24;
+
+
+// console.log(price + "is expensive ");
+// console.log(`you are ${age} year old`);
+// console.log("your total score is "+marks);
+
+// // introduction to datatypes - String :
+// let FirstName = `Suraj`;
+// let LastName ="Jondhale";
+// let Fullname=`${FirstName} ${LastName}`;
+
+// console.log( Fullname);
+
+
+// // booleeans : (19:36)-->
+
+// let online = false ;
+// let forSale = true ;
+
+// // console.log(`bro is online :${online}`);
+// console.log(`is this car for sale : ${forSale}`);
+
+//mix data types :(22:39)
+
+// let FullName = " Suraj S. Jondhale";
+// let age = 24 ;
+// let inschool = false ;
+
+// document.getElementById("p1").textContent = `Your name is ${FullName}`;
+// document.getElementById( "p2").textContent = `Your age is ${age} years `;
+// document.getElementById("p3").textContent = `Enrolled In School : ${inschool}` ;
+
+
+
+
+
+// arthematic operator = operator (values , variables , etc ) 26:52
+//  assignment operators (=,+=,-+,*=/)
+
+// let students = 30;
+
+// students = students -1;
+// students = students +1;
+// students = students *2;
+// students = students /2;
+// students =students**3;
+// students = students % 3;
+
+// console.log(students);
+
+// let result = 1+2*3+4**2; 
+
+// console.log(result);
+
+                        //  operator procedure : 
+                        // paranthesis ()
+                        //  exponentiation **
+                        //  multiplication * division & modulo /
+                        // addition and subtraction + -
+
+
+// 32:35
+
+// user input
+ 
