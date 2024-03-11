22bd884bf3b1a0a6fa8c99a873db5b013d648f48
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-03-11 13:12:08 +0530

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,15 @@
+//  type conversion = change the datatypes of a values to another data types 
+//  string , numbers , boolens
+
+let x = "banana";
+let y = "banana";
+let z = "banana";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
