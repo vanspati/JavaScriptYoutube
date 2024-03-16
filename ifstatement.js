0a5efbd9ca26c829c4e001df98a266e998c104e0
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-03-16 14:06:09 +0530

    Add files via upload

---
+++ ifstatement.js
@@ -0,0 +1,12 @@
+// its true something yells
+// its false  nothing yells
+
+// if statements  are used to execute a block of code based on
+
+let age =19;
+if(age >= 18){
+    console.log("You can vote");
+}
+else{
+    console.log("ghari ja bala ")
+}
