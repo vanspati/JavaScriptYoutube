2a1197dcb0c56496c550254d55c5856219c231c8
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-03-17 22:46:06 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,12 @@
+// ternary operator  = a shortcut to if() and else() statement
+                    // helps to assigne a variable based on a condition 
+                    // condition ? codeIfTrue : codeIfFalse ;
+
+// let age = 21;
+// let message = age >=45 ? "you are adult " : "you are minor" ;
+// console.log(message);
+
+ 
+// let time = 16 ;
+// let greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+// console.log(greeting);
