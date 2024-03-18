c6e3e745f24fe1f0aea347408ce6cfddbb5d0465
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-03-18 15:32:35 +0530

    Add files via upload

---
+++ stringmethod.js
@@ -0,0 +1,33 @@
+// string method :
+
+let username = "JondhaleSuraj";
+let phonenumber = "12-34-567-890";
+
+phonenumber =  phonenumber.replaceAll("-", "");
+console.log(phonenumber);
+
+username.charAt(0);  
+console.log(username.charAt(0));
+
+
+username.indexOf("S");  
+console.log(username.indexOf("e"));
+
+username.lastIndexOf( "l" ); 
+console.log(username.lastIndexOf("l"));
+
+username.length;
+console.log(username.length);
+
+username.toLowerCase();
+console.log(username.toLowerCase());
+
+username.toUpperCase()
+console.log(username.toUpperCase());
+
+username.trim();
+console.log(username.trim(1,2)); 
+
+username.startsWith();
+console.log(username.startsWith('J'));
+
