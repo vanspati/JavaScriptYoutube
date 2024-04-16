ebf1112d5cca0ce5a860bf320fe7ae03805cf2e9
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-04-16 16:55:20 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+// function happyBirthday(username , age){
+//     console.log("Happy Birthday to you seth");
+//     console.log(`Happy Birthday to you dear ${username}`);
+//     console.log(`you are ${age} year old `);
+// }
+// happyBirthday("suraj" , 23 );
+// happyBirthday("mark" ,45);
+// happyBirthday("alex" , 20);
+
+
+// function : a section of reusable code 
+// declare code , ise it whenaever you want .
+// call the function to sxcept that code .
+function add(x,y){
+    let result = x+y;
+    return result ;
+
+}
+let answer = add(5,5);
+console.log(answer);
