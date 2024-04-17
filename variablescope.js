bf8ccecb44d7883ac1b8b8ff1c8f762c1650518a
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-04-17 10:54:10 +0530

    Add files via upload

---
+++ variablescope.js
@@ -0,0 +1,11 @@
+// variable scope = where a variable is recognised and accesible (local vs global) 
+
+function1()
+function function1(){
+let x = 1 ;
+console.log(x);
+}
+function function2(){
+let x = 2 ;
+console.log(x);
+}
