46478db998e397bf6d36f606574a5e6df1f1ddaa
vanspati_23 <132640744+vanspati@users.noreply.github.com>
2024-04-17 17:09:41 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+// Array = a variable like structure that can hold multiple values
+// Array = [value0, value1, value2]
+
+let fruit = ["Safrchnd" , "Keli" , "Santre" , "Naral"];
+
+// fruit.push("naral");
+// fruit.pop();
+// fruit.unshift("mango");
+// fruit.shift();
+
+let numOfFruits = fruit.length;
+let index = fruit.indexOf("Santre");
+
+console.log(index);
